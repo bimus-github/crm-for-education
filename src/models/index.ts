e341e65e5404a1d4c9a3f45fad3966bce20386a4
serdarchik2001 <sardorbekaminjonov2001@gmail.com>
2022-05-07 15:05:50 +0500

    Added Types

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,57 @@
+export enum ROLE {
+  TEACHER,
+  STUDENT,
+  ADMIN,
+}
+
 export type User = {
+  id: string;
   firstName: string;
   lastName: string;
   email: string;
+  phone: string;
+  role: ROLE;
+};
+
+export type School = {
+  id: string;
+  logo: string;
+  name: string;
+  teachers: string[];
+};
+
+export type Student = User & {};
+
+export type Teacher = User & {
+  groups: string[];
+  languages: string[];
+};
+
+export type Day =
+  | 'Monday'
+  | 'Tuesday'
+  | 'Wednesday'
+  | 'Thursday'
+  | 'Friday'
+  | 'Saturday'
+  | 'Sunday';
+
+export type Group = {
+  id: string;
+  name: string;
+  language: string;
+  monthlyBill: number;
+  subject: string;
+  teacher: {
+    user: string;
+    monthlyBillPercentage: string;
+  };
+  students: string;
+  schedule: {
+    days: Day[];
+    time: {
+      start: number;
+      end: number;
+    };
+  };
 };
