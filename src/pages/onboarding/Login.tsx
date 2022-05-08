2540bfbfdff7945312314d34f35e48587ee41986
Madaminjon <105053570+bimus-github@users.noreply.github.com>
2022-05-08 19:45:32 +0500

    styling login-screen (#2)
    Co-authored-by: Madaminjon Abdullajonov <musulmon20010716@gmail.com>

--- src/pages/onboarding/Login.tsx
+++ src/pages/onboarding/Login.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-} from 'firebase/auth';
-import { auth } from '../../lib/firebase/init';
-import { Link } from 'react-router-dom';
-import toast from 'react-hot-toast';
+} from "firebase/auth";
+import { auth } from "../../lib/firebase/init";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+
+import { BsPerson } from "react-icons/bs";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignUpPress = () => {
     createUserWithEmailAndPassword(auth, email, password)
@@ -35,52 +38,57 @@ const Login = () => {
         // ...
       })
       .catch((error) => {
-        toast.error('Error to sign up');
+        toast.error("Error to sign up");
         const errorCode = error.code;
         const errorMessage = error.message;
       });
   };
 
   return (
-    <div className='h-screen w-screen flex justify-center items-center bg-app-background'>
-      <form onSubmit={handleSubmit} className='space-y-3'>
-        <div className='mb-20'>
-          <div>
+    <div className="h-screen w-screen flex justify-center items-center bg-app-background">
+      <form onSubmit={handleSubmit} className="space-y-10">
+        <div className="  flex flex-col gap-5">
+          <div className=" flex">
+            <BsPerson className=" rounded-l-md h-[40px] w-[40px] bg-app-primary " />
             <input
-              placeholder='Email'
-              type='text'
+              placeholder="Username"
+              type="text"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              name=''
-              id=''
-              className='h-[50px] w-[250px] px-3'
+              name=""
+              id=""
+              className="h-[40px] w-[300px] bg-white px-3 rounded-r-md"
             />
           </div>
-          <div>
+          <div className=" flex">
+            <RiLockPasswordLine className=" rounded-l-md h-[40px] w-[40px]  bg-app-primary" />
+
             <input
-              placeholder='Password'
-              type={'password'}
+              placeholder="Password"
+              type={"password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              name=''
-              id=''
-              className='h-[50px] w-[250px] px-3'
+              name=""
+              id=""
+              className="h-[40px] w-[300px] px-3 rounded-r-md"
             />
           </div>
         </div>
 
-        <div className='flex flex-col justify-center items-center'>
+        <div className="flex flex-col justify-center items-center">
           <div>
             <button
-              type='submit'
-              className='flex items-center justify-center bg-app-primary h-[50px] w-[200px] rounded-[5px] text-white text-lg'
+              type="submit"
+              className="flex items-center text-black justify-center hover:bg-red-400 bg-app-primary h-[40px] w-[340px] rounded-[5px]   text-lg"
             >
-              Login{' '}
+              Login
             </button>
 
-            <Link to='/signUp'>
-              <p className='text-center'> Go to Sing Up</p>
-            </Link>
+            <div className=" h-[40px] w-[340px] flex items-center justify-end">
+              <Link to="/signUp">
+                <p className="text-center text-blue-600"> Go to Sing Up</p>
+              </Link>
+            </div>
           </div>
         </div>
       </form>
