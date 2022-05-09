ff0e165348055ccaaa9110c4ff8466fbe7b20a8a
serdarchik2001 <sardorbekaminjonov2001@gmail.com>
2022-05-09 15:13:10 +0500

    Updated table to be beautiful

--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AppLayout from "src/components/shared/layout";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppLayout from 'src/components/shared/layout';
 
 const TeachersPage = () => {
   return (
     <AppLayout>
-      <div className=" p-5 w-full h-screen bg-app-background flex flex-col gap-2">
-        <div className=" w-full h-auto flex justify-between items-center">
+      <div className='p-5 w-full flex flex-col gap-2'>
+        <div className=' w-full h-auto flex justify-between items-center mb-3'>
           <input
-            className="w-[200px] h-[30px] rounded-md pl-2 outline-none ring-[1px] focus:ring-app-primary focus:ring-offset-2 hover:ring-blue-400"
-            type="text"
-            placeholder="Search"
+            className='w-[200px] h-[40px] rounded-[5px] pl-2 outline-none ring-[1px] focus:ring-app-primary focus:ring-offset-2 hover:ring-blue-400'
+            type='text'
+            placeholder='Search'
           />
-          <Link to={"./createTeacher"}>
-            <button className=" w-[200px] h-[30px] rounded-md bg-app-secondary hover:bg-app-secondary-lighter text-white  ">
+          <Link to={'/create/teacher'}>
+            <button className='w-[200px] h-[40px] rounded-md bg-app-secondary hover:bg-app-secondary-lighter text-white font-bold'>
               Add new teacher
             </button>
           </Link>
         </div>
-        <div className=" p-1 w-full h-full bg-white">
-          <div className=" w-full h-[30px] flex items-center border-b-2">
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+        <div className='p-1 w-full min-h-[600px] bg-white shadow-md rounded-[5px]'>
+          <div className='w-full flex items-center border-b py-3'>
+            <div className=' mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               First Name
             </div>
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               Last Name
             </div>
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               Num. of groups
             </div>
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               Email
             </div>
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               Started date
             </div>
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[1] h-full"></div>
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[1] h-full'></div>
           </div>
         </div>
       </div>
