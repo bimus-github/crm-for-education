5307dae7f0fe40aeb8469c2b91f3348280a8c15e
serdarchik2001 <sardorbekaminjonov2001@gmail.com>
2022-05-09 20:39:34 +0500

    Get user doc

--- src/lib/firebase/services/user.ts
+++ src/lib/firebase/services/user.ts
@@ -1,5 +1,5 @@
 import { User } from 'src/models';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, where, query, getDocs } from 'firebase/firestore';
 import { firestore } from '../init';
 
 export const USERS_PATH = 'users';
@@ -8,4 +8,22 @@ export const createUser = async (user: User) => {
   const docRef = await addDoc(collection(firestore, USERS_PATH), user);
 
   return docRef.id;
+};
+
+export const getCurrentUser = async (user: string) => {
+  let userDoc: User | null = null;
+
+  const q = query(
+    collection(firestore, USERS_PATH),
+    where('email', '==', user)
+  );
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, ' => ', doc.data());
+    userDoc = doc.data() as User;
+  });
+
+  return userDoc;
 };
--- src/lib/hooks/useFirebaseAuthentication .ts
+++ src/lib/hooks/useFirebaseAuthentication .ts
@@ -1,19 +1,39 @@
 import { User } from 'firebase/auth';
 import { useEffect, useState } from 'react';
+import { UserActions } from 'src/store/features/user';
+import { useAppDispatch } from 'src/store/hooks';
 import { auth } from '../firebase/init';
+import { getCurrentUser } from '../firebase/services/user';
 
 const useFirebaseAuthentication = () => {
+  const dispatch = useAppDispatch();
+
+  const [loading, setLoading] = useState(false);
   const [authUser, setAuthUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unlisten = auth.onAuthStateChanged((authUser) => {
-      authUser ? setAuthUser(authUser) : setAuthUser(null);
+    setLoading(true);
+
+    const unlisten = auth.onAuthStateChanged(async (authUser) => {
+      if (authUser) {
+        setLoading(false);
+
+        const userDoc = await getCurrentUser(authUser.email as string);
+
+        userDoc && dispatch(UserActions.setUser(userDoc));
+
+        return setAuthUser(authUser);
+      }
+
+      setAuthUser(null);
+      setLoading(false);
+      // authUser ? setAuthUser(authUser) : setAuthUser(null);
     });
 
     return () => unlisten();
   }, []);
 
-  return authUser;
+  return { authUser, loading };
 };
 
 export default useFirebaseAuthentication;
