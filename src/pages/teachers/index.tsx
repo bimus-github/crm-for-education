36f1b4398577b4a1d14c23ff39327065279e37ff
serdarchik2001 <sardorbekaminjonov2001@gmail.com>
2022-05-21 21:35:56 +0500

    User removeUser action

--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AppLayout from "src/components/shared/layout";
-import { Language, ROLE, User } from "src/models";
-import { useAppSelector } from "src/store/hooks";
-import moment from "moment";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AppLayout from 'src/components/shared/layout';
+import { Language, ROLE, User } from 'src/models';
+import { useAppDispatch, useAppSelector } from 'src/store/hooks';
+import moment from 'moment';
 
-import { BsPencilSquare } from "react-icons/bs";
-import { MdDelete } from "react-icons/md";
-import { deleteUser } from "src/lib/firebase/services/user";
-import toast from "react-hot-toast";
-import { deleteGroup } from "src/lib/firebase/services/group";
+import { BsPencilSquare } from 'react-icons/bs';
+import { MdDelete } from 'react-icons/md';
+import { deleteUser } from 'src/lib/firebase/services/user';
+import toast from 'react-hot-toast';
+import { deleteGroup } from 'src/lib/firebase/services/group';
+import { UserSliceActions } from 'src/store/features/users';
 
 const TeachersPage = () => {
+  const dispatch = useAppDispatch();
   const usersSlices = useAppSelector((i) => i.usersSlice);
 
   const groupSlices = useAppSelector((i) => i.groupsSlice);
@@ -47,63 +49,64 @@ const TeachersPage = () => {
           deleteUser(deletedStudents[i].id);
         }
       }
-      toast.success("Deleting has been successfully");
+      dispatch(UserSliceActions.removeUser(t));
+      toast.success('Deleting has been successfully');
     } catch (error) {
-      toast.error("Error, while deleting");
+      toast.error('Error, while deleting');
     }
   };
 
   return (
     <AppLayout>
-      <div className="p-5 w-full flex flex-col gap-2">
-        <div className=" w-full h-auto flex justify-between items-center mb-3">
+      <div className='p-5 w-full flex flex-col gap-2'>
+        <div className=' w-full h-auto flex justify-between items-center mb-3'>
           <input
-            className="w-[200px] h-[40px] rounded-[5px] pl-2 outline-none ring-[1px] focus:ring-app-primary focus:ring-offset-2 hover:ring-blue-400"
-            type="text"
-            placeholder="Search"
+            className='w-[200px] h-[40px] rounded-[5px] pl-2 outline-none ring-[1px] focus:ring-app-primary focus:ring-offset-2 hover:ring-blue-400'
+            type='text'
+            placeholder='Search'
           />
-          <Link to={"/create/teacher"}>
-            <button className="w-[200px] h-[40px] rounded-md bg-app-secondary hover:bg-app-secondary-lighter text-white font-bold">
+          <Link to={'/create/teacher'}>
+            <button className='w-[200px] h-[40px] rounded-md bg-app-secondary hover:bg-app-secondary-lighter text-white font-bold'>
               Add new teacher
             </button>
           </Link>
         </div>
-        <div className="p-1 w-full min-h-[600px] bg-white shadow-md rounded-[5px]">
-          <div className="w-full flex items-center border-b py-3">
-            <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+        <div className='p-1 w-full min-h-[600px] bg-white shadow-md rounded-[5px]'>
+          <div className='w-full flex items-center border-b py-3'>
+            <div className=' mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               First Name
             </div>
-            <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               Last Name
             </div>
-            <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full'>
               Num. of groups
             </div>
-            <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
               Email
             </div>
-            <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full'>
               Started date
             </div>
-            <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[1] h-full">
+            <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[1] h-full'>
               Change
             </div>
           </div>
           {teachers.map((t) => (
-            <div className="p-1 w-full h-auto bg-white  rounded-[5px] flex items-center  py-3">
-              <div className=" mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
-                <div className=" w-[30px] h-[30px] mr-1">
+            <div className='p-1 w-full h-auto bg-white  rounded-[5px] flex items-center  py-3'>
+              <div className=' mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
+                <div className=' w-[30px] h-[30px] mr-1'>
                   <img
                     src={t.img}
-                    className=" h-full w-full rounded-full hover:h-[200px] hover:w-[200px] hover:absolute hover:shadow-md"
+                    className=' h-full w-full rounded-full hover:h-[200px] hover:w-[200px] hover:absolute hover:shadow-md'
                   />
                 </div>
                 {t.firstName}
               </div>
-              <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+              <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
                 {t.lastName}
               </div>
-              <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full">
+              <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full'>
                 {
                   groups?.filter(
                     (l) =>
@@ -112,17 +115,17 @@ const TeachersPage = () => {
                   )?.length
                 }
               </div>
-              <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full">
+              <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[3] h-full'>
                 {t.email}
               </div>
-              <div className="mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full">
-                {moment(Number(t.startedTime)).format("MMMM Do YYYY")}
+              <div className='mx-1 text-base flex items-center justify-start pl-2 font-bold flex-[2] h-full'>
+                {moment(Number(t.startedTime)).format('MMMM Do YYYY')}
               </div>
-              <div className="mx-1 text-base flex items-center justify-around pl-2 font-bold flex-[1] h-full">
-                <BsPencilSquare className=" h-[20px] w-[20px] hover:text-app-secondary hover:w-[30px] hover:h-[30px]" />
+              <div className='mx-1 text-base flex items-center justify-around pl-2 font-bold flex-[1] h-full'>
+                <BsPencilSquare className=' h-[20px] w-[20px] hover:text-app-secondary hover:w-[30px] hover:h-[30px]' />
                 <MdDelete
                   onClick={() => onDelete(t)}
-                  className=" h-[20px] w-[20px] hover:text-red-500 hover:w-[30px] hover:h-[30px]"
+                  className=' h-[20px] w-[20px] hover:text-red-500 hover:w-[30px] hover:h-[30px]'
                 />
               </div>
             </div>
